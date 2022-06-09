9d94de666fae24f040235016836dce83c4610e71
1cosmic <notifylore@gmail.com>
2022-06-09 21:09:15 +0400

    Draw grid

--- source/graphic/draw.cpp
+++ source/graphic/draw.cpp
@@ -5,16 +5,29 @@ void draw_grid(SDL_Renderer *render, int start_x, int start_y, int indent) {
 
   int x = start_x;
   int y = start_y;
+  int draw = start_y;
 
   int endLine = start_x + (9 * indent);
 
-  int speed = (endLine - start_x) / 100;
+  int speed = (endLine - start_x) / 60;
 
-  for (x = start_x; x < endLine; x += speed) {
-    SDL_RenderDrawLine(render, start_x, start_y, x, y);
+  int line; // index of line.
+
+  for (draw = 0; draw < endLine - start_x; draw += speed) {
+
+    for (line = 1; line < 9; ++line) {
+
+      // Draw vertical line.
+      SDL_RenderDrawLine(render, start_x + (indent * line), start_y,
+                         x + (indent * line), y + draw);
+
+      // Draw horisontal line.
+      SDL_RenderDrawLine(render, start_x, start_y + (indent * line), x + draw,
+                         y + (indent * line));
+    }
+
     SDL_RenderPresent(render);
-
-    // speed *= 0.9;
+    // speed *= 0.8;
 
     SDL_Delay(10);
   }
